2c467f8f1ba7c497852a7a4ef833b8e46ebd8747
a-basuony <basouny85@gmail.com>
2022-11-14 20:37:13 +0200

    create react project

---
+++ src/data/data.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-dupe-keys */
+export const data=[
+    {
+        id: 1,
+        image: "https://m.media-amazon.com/images/I/71JOD+NF3GL._AC_UL320_.jpg",
+        name: "HP Laptop",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$600",
+        rating: 5,
+        numRev: 11,
+        stock: 16
+    },
+    {
+        id: 2,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeNUClUTkvwZ_UO7FHKRoSwVGl2KwonHNB-A&usqp=CAU",
+        name: "Lenovo Laptop",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$580",
+        rating: 4.5,
+        numRev: 25,
+        stock: 20
+    },
+    {
+        id: 3,
+        image: "https://images-eu.ssl-images-amazon.com/images/I/61dWmHHqKyL._AC_UL232_SR232,232_.jpg",
+        name: "Mouse",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$350",
+        rating: 3,
+        numRev: 46,
+        stock: 2
+    },
+    {
+        id: 4,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhX93oPl7ke3pW30m6Bf8yuVtZXLztiDg4Jw&usqp=CAU",
+        name: "Monitor",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$270",
+        rating: 4.5,
+        numRev: 17,
+        stock: 9
+    },
+    {
+        id: 5,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhX93oPl7ke3pW30m6Bf8yuVtZXLztiDg4Jw&usqp=CAU",
+        name: "Mobile",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$300",
+        rating: 2,
+        numRev: 33,
+        stock: 0
+    },
+    {
+        id: 6,
+        image: "https://i.pcmag.com/imagery/roundups/007y4PjTbtgMMDz5SnUq9Em-1.fit_lim.size_1838x.jpg",
+        name: "Keyboard",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$120",
+        rating: 4,
+        numRev: 20,
+        stock: 50
+    },
+    {
+        id: 7,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCRrVj0auKueDwvlAc6czV92ee1kkXYjDSGQ&usqp=CAU",
+        name: "Earphone",
+        rating: "5",
+        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        price: "$90",
+        rating: 5,
+        numRev: 14,
+        stock: 4
+    }
+]
